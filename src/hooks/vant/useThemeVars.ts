8cdaab6a02088a12a7f04b03521a067972e045b0
plasticine9750 <975036719@qq.com>
2022-05-08 08:20:15 +0800

    ci(vant css vars): customize vite css variables

---
+++ src/hooks/vant/useThemeVars.ts
@@ -0,0 +1,9 @@
+/**
+ * @description 自定义 vant css 组件变量 全局变量需要到 src/style/vant.scss 中定义
+ * @returns themeVars
+ */
+export const useThemeVars = () => {
+  return {
+    buttonPrimaryBackgroundColor: '#292d3e',
+  };
+};
