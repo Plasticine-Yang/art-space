905415244eab9f61b82e9e6c439429f45ecada1d
plasticine9750 <975036719@qq.com>
2022-05-10 17:52:29 +0800

    feat(component): encapsulate logo-title component

---
+++ src/components/LogoTitle.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import { useEnv } from '@build/utils';
+  import { Row, Col } from 'vant';
+
+  // 网站标题
+  const { VITE_GLOB_APP_TITLE: title } = useEnv(import.meta.env);
+</script>
+
+<template>
+  <Row align="center" gutter="4">
+    <Col>
+      <van-icon name="smile" size="2rem" color="#B8F2E6" />
+    </Col>
+    <Col>
+      <h1 class="m-0 tracking-widest font-bold"> {{ title }} </h1>
+    </Col>
+  </Row>
+</template>
