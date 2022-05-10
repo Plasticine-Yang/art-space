f44a2d75e59fb03b55831774fae83d63a0922fc8
plasticine9750 <975036719@qq.com>
2022-05-10 17:58:32 +0800

    refactor(home): use detached logo-title component in home page

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,29 +1,13 @@
 <script setup lang="ts">
-  import { useEnv } from '@build/utils';
-  import { Row, Col } from 'vant';
-
   import Swipe from './Swipe.vue';
   import Tabs from './Tabs.vue';
-
-  // ==================== data ====================
-
-  // 网站标题
-  const { VITE_GLOB_APP_TITLE: title } = useEnv(import.meta.env);
-
-  // ==================== methods ====================
+  import LogoTitle from '@/components/LogoTitle.vue';
 </script>
 
 <template>
   <main class="box-border py-6 px-4">
     <!-- 网站标题 logo -->
-    <Row align="center" gutter="4">
-      <Col>
-        <van-icon name="smile" size="2rem" color="#B8F2E6" />
-      </Col>
-      <Col>
-        <h1 class="m-0 tracking-widest font-bold"> {{ title }} </h1>
-      </Col>
-    </Row>
+    <LogoTitle />
 
     <div class="flex flex-col gap-2 mt-8">
       <!-- 轮播图 -->
