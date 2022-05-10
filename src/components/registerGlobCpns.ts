5b16f881fb4718f70ef3f33dc93851f06b5a99dc
plasticine9750 <975036719@qq.com>
2022-05-10 21:33:50 +0800

    ci(global component): register vant row and col as global component

--- src/components/registerGlobCpns.ts
+++ src/components/registerGlobCpns.ts
@@ -1,7 +1,7 @@
 import { App } from 'vue';
 
-import { Icon, ConfigProvider, Lazyload, Image } from 'vant';
+import { Icon, ConfigProvider, Lazyload, Image, Row, Col } from 'vant';
 
 export function registerGlobCpns(app: App) {
-  app.use(Icon).use(ConfigProvider).use(Lazyload).use(Image);
+  app.use(Icon).use(ConfigProvider).use(Lazyload).use(Image).use(Row).use(Col);
 }
