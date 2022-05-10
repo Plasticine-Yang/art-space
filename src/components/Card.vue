07f84b6d19a5c52d3d4439776eba7c49b89f02f5
plasticine9750 <975036719@qq.com>
2022-05-10 17:52:04 +0800

    feat(component): encapsulate card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <Row class="card bg-tabbar p-4 gap-4 text-main-text-color">
+  <Row class="card">
     <!-- 头部 -->
     <slot name="header"></slot>
 
@@ -12,18 +12,35 @@
 </template>
 
 <script setup lang="ts">
+  import { mainTextColor, tabbarColor } from '@/config/vant/themeConfig';
   import { Row } from 'vant';
   const props = defineProps<{
     // 是否圆角显示
     rounded?: boolean;
+    // 内边距
+    padding?: string;
+    // 三部分之间的间距
+    gap?: string;
+    // 卡片背景颜色
+    backgroundColor?: string;
+    // 字体颜色
+    textColor?: string;
   }>();
 
   // style
   const roundedStyle = props.rounded ? '1rem' : '0';
+  const paddingStyle = props.padding ?? '1rem';
+  const gapStyle = props.gap ?? '1rem';
+  const bgColorStyle = props.backgroundColor ?? tabbarColor;
+  const textColorStyle = props.textColor ?? mainTextColor;
 </script>
 
 <style scoped lang="scss">
   .card {
     border-radius: v-bind(roundedStyle);
+    padding: v-bind(paddingStyle);
+    gap: v-bind(gapStyle);
+    background-color: v-bind(bgColorStyle);
+    color: v-bind(textColorStyle);
   }
 </style>
